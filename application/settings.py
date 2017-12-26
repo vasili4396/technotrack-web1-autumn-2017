dab71895f0e23505d2c50a6b08a206fce3438bd8
vasiliy <vasili4396@mail.ru>
2017-12-26 19:46:11 +0300

    upd

--- application/settings.py
+++ application/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 )
 
 ROOT_URLCONF = 'application.urls'
